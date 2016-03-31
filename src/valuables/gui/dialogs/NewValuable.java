a9ec6546ceebc40163aec9a89b6de96c8728a9b3
Fredrik <flarsson93@gmail.com>
2016-03-31 11:54:43 +0200

    add newValuable dialog class

--- src/valuables/gui/dialogs/NewValuable.java
+++ src/valuables/gui/dialogs/NewValuable.java
@@ -1,91 +1,25 @@
 package valuables.gui.dialogs;
 
-import java.awt.Dialog.ModalityType;
-import java.awt.Dimension;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import valuables.gui.GUI;
+import valuables.items.Valuable;
 
-public class NewValuable{//FIXME Rewrite - use JOption?Dialog?
+@SuppressWarnings("serial")
+public abstract class NewValuable extends JOptionPane{
+	private JLabel nameText = new JLabel("Name:");
+	private JTextField nameInput = new JTextField();
 	
-	private int frameWidth = 300;
-	private int frameHeight = 300;
-	
-	private GUI gui;
-	
-	private JDialog window;
-	private JPanel panel;
-	private JLabel nameLabel;
-	private JTextField nameInput;
-	private JButton ok,cancel;
-	private GridBagLayout panelLayout;
-	private GridBagConstraints textConstraints,inputConstraints;
-	
-	public NewValuable(GUI gui){
-		this.gui = gui;
-		initialize();		
+	public NewValuable(){
+		JPanel namePanel = new JPanel(new BoxLayout(this, BoxLayout.Y_AXIS));
+		namePanel.add(nameText);
+		namePanel.add(nameInput);
 	}
-	private void initialize(){
-		//JDialog.setDefaultLookAndFeelDecorated(true);
-		window = new JDialog(gui.getFrame());
-		panelLayout = new GridBagLayout();
-		panel = new JPanel(panelLayout);
-		addFields();
-		window.add(panel);
-		window.pack();
-		setWindowOptions();
+	public String getName(){
+		return nameInput.getSelectedText();
 	}
-	private void setWindowOptions(){
-		window.setTitle("New "+ getClassName());
-		window.setSize(frameWidth, frameHeight);
-		window.setResizable(false);
-		window.setModalityType(ModalityType.APPLICATION_MODAL);
-		window.setVisible(true);
-		centerFrameOnDefaultMonitor();		
-	}
-	private void centerFrameOnDefaultMonitor(){
-		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
-		int width = gd.getDisplayMode().getWidth();
-		int height = gd.getDisplayMode().getHeight();
-		int frameLocationX = (int) ((width - frameWidth)/2); //Center the window on the X axis
-		int frameLocationY = (int) ((height - frameHeight)/2); //Center the window on the Y axis		
-		window.setLocation(frameLocationX,frameLocationY);
-	}
-	private String getClassName(){
-		String fullClassName = this.getClass().getName();
-		return fullClassName.substring(fullClassName.lastIndexOf(".New")+4).toLowerCase();
-	}
-	private void addFields(){
-		setConstraints();
-		addNameField();
-	}
-	private void addNameField(){		
-		nameLabel = new JLabel("Name:");
-		panel.add(nameLabel,textConstraints);
-		nameInput = new JTextField();
-		nameInput.setPreferredSize(new Dimension(150,20));
-		panel.add(nameInput,inputConstraints);
-	}
-	private void setConstraints(){
-		textConstraints = new GridBagConstraints();
-		textConstraints.anchor = GridBagConstraints.EAST;
-		textConstraints.insets = new Insets(0,0,5,2);
-		
-		inputConstraints = new GridBagConstraints();
-		inputConstraints.insets = new Insets(0,2,5,0);
-		inputConstraints.anchor = GridBagConstraints.WEST;
-		inputConstraints.fill = GridBagConstraints.EAST;
-		inputConstraints.gridx = 1;
-		inputConstraints.weightx = 1;
-	}	
+	public abstract Valuable getValuable();
 }
